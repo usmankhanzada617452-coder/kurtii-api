9e5b2526797a9210b792319a0f6b00a351cf6fab
USMAN KHAN <usmankhanzada617452@gmail.com>
2026-09-06 21:04:47 +0500

    Added admin authorization to product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,14 +7,12 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productController");
+const { protect, adminOnly } = require("../middleware/auth");
 
-// /api/products
 router.get("/", getProducts);
-router.post("/", createProduct);
-
-// /api/products/:id
 router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.post("/", protect, adminOnly, createProduct);
+router.put("/:id", protect, adminOnly, updateProduct);
+router.delete("/:id", protect, adminOnly, deleteProduct);
 
 module.exports = router;
